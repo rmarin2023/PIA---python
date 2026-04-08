15db4785d8f164df09dcec0ac4f720573be23ef5
Ruben Marin Silva <131864575+rmarin2023@users.noreply.github.com>
2026-04-08 08:19:34 +0000

    ultima coipa

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-print("hola a todos")
-nombre = input("Dame tu nombre: " )
-print("Tu nombre es : " +nombre)
+numeros = []
+
+for i in range(3):
+    n = int(input("Escriba un número: "))
+    numeros.append(n)
+
+# Eliminamos duplicados y obtenemos la cantidad de números que hay
+son_iguales = len(set(numeros))
+match son_iguales:
+    case 1:
+        print("Todos los números son iguales")
+    case 2:
+        print("Dos de los números son iguales")
+    case 3:
+        print("Todos los números son diferentes")
